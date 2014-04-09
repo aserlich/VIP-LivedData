6440bfba5e64e916856869bbf28b5c530f9d13b3
Aaron Erlich <aaron.erlich@gmail.com>
2014-04-09 12:05:43 +0200

    Updated the out output message to be more intellegible and also changed the file to include the second address field in the subset csv created by the file.

--- ErrorChecksViz.r
+++ ErrorChecksViz.r
@@ -1,5 +1,8 @@
 library(stringr)
 library(plyr)
+library(xts) # also pulls in zoo
+library(timeDate)
+library(chron)
 ##Detect problems with ward lookup
 #############################
 workd <- "/Volumes/Optibay-1TB/RSA_RCT/QA/LiveData/VIP-LivedData/"
@@ -9,12 +12,12 @@ exports <- list.files(path=workd, pattern ="contact_2014_[0-9].*")
 currentFile <-  tail(exports,1)[1]
 setwd(paste0(workd,currentFile))
 
-cat("Now loading the exports file from most recent file", currentFile)
+cat("Now loading the exports file from most recent file", currentFile, "\n\n")
 
 exportFile <- read.csv("contacts-export.csv", header=TRUE, stringsAsFactors=FALSE,  na.strings="")
 
 numObs <- nrow(exportFile)
-numAdd <- nrow(exportFile[!is.na(exportFile$extras.ward), ])
+numAdd <- nrow(exportFile[!is.na(exportFile$extras.raw_user_address), ])
       
 #String match
 #exportFile$extras.raw_user_address
@@ -31,19 +34,22 @@ exportFile$wellFormed <- unlist(sapply(exportFile$extras.raw_user_address, simpl
       ))
 
 #Create subset
-addressSubset <- exportFile[(exportFile$wellFormed==1 | !is.na(exportFile$extras.ward)),
-           c("extras.raw_user_address", "extras.ward", "wellFormed")]
+addressSubset <- exportFile[( !is.na(exportFile$extras.raw_user_address)| !is.na(exportFile$extras.ward)),
+           c("extras.raw_user_address","extras.raw_user_address_2", "extras.ward", "wellFormed")]
 
 #Digit fields
 numGoodWards <- length(na.omit(addressSubset$extras.ward))
-numGoodAddresses <-nrow(addressSubset[!is.na(addressSubset$extras.raw_user_address), ])
-mismatch1 <- nrow(addressSubset[addressSubset$wellFormed==0 & !is.na(addressSubset$extras.ward), ])
+numGoodAddresses <-sum(addressSubset$wellFormed[!is.na(addressSubset$extras.ward)], na.rm =TRUE)
+wfform <- nrow(addressSubset[addressSubset$wellFormed==1 & !is.na(addressSubset$extras.ward), ])
 mismatch2 <- nrow(addressSubset[addressSubset$wellFormed==1 & is.na(addressSubset$extras.ward), ])
 
-cat(sprintf("The dataset currently has %d observations, of which %d have filled out the address field\n", numObs, numAdd))
-cat(sprintf("There are currently %d addresses where the ward looked up went through
-and %d addresses that meet basic formatting requirements of **number** then **text**
- There are currently %d cases where the address is not well formed but there is still a ward match and %d where the address is well formed but there is not a match", numGoodWards, numGoodAddresses, mismatch1, mismatch2))
+cat(sprintf("The dataset currently has %d observations, of which %d have filled out the address field, which is %f", numObs, numAdd, numAdd/numObs), "\n\n")
+cat(sprintf("Of the addresses, there are currently %d addresses where the ward looked up went through
+of those, %d addresses that meet basic formatting requirements of **number** then **text**, which constitutes %f of all ward matches.
+
+There are currently %d cases where the address is not well formed but there is still a ward match
+and %d where the address is well formed but there is not a match.",
+            numGoodWards, numGoodAddresses, numGoodAddresses/numGoodWards, wfform, mismatch2))
 
 #output a csv of the address concerns
 write.csv(addressSubset, file=paste0("AddressSubset","_", Sys.time(), ".csv"))
@@ -53,4 +59,12 @@ write.csv(addressSubset, file=paste0("AddressSubset","_", Sys.time(), ".csv"))
 ###############################
 ##visualize count of incoming messages by hour
 ##################################
+
+#x <- as.xts(1:3, timeDate(exportFile$created_at))
+
+
+#strptime(exportFile$created_at[1], "Y%-%m-%d %H:%M")
+
+
+
 setwd(workd)
