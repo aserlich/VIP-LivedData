6b06964d768542b913b0a8b42feb0c258e445bfc
Aaron Erlich <aaron.erlich@gmail.com>
2014-04-14 15:57:46 +0200

    First thoughts on fixing bad ward data

---
+++ fixWards.r
@@ -0,0 +1,13 @@
+##Aaron Erlich
+##Thoughts
+
+
+##Run the code against Adi's flags
+## Remove the wards where nothing is returned
+##These people will be randomized in some way for phase 2b if we cannot use any of the other smart guesses below
+
+#There are a bunch of observations where people give us their ward # and also some information of where they Live
+#see if we can rescue those by calling Adi's api without the ward info striped and then add back the ward number
+#If just ward is present, we have to remove this observation
+
+#Remove everyone that entered the example 12 main street pretoria
