a5bd716a6b2505ed4bb4f4a6cfd83ec66a29f759
Aaron Erlich <aaron.erlich@gmail.com>
2014-04-14 16:40:15 +0200

    Bug fix to make sure was delivered through USSD and right csv was used

--- sampleWardNums.r
+++ sampleWardNums.r
@@ -25,18 +25,28 @@ cat("Now loading the exports file from most recent file", currentFile, "\n\n")
 
 exportFile <- read.csv("contacts-export.csv", header=TRUE, stringsAsFactors=FALSE,  na.strings="")
 
-recontact <- exportFile[exportFile$extras.is_registered %in% c("true")  & is.na(exportFile$extras.raw_user_address), ]
+recontact <- exportFile[exportFile$extras.is_registered %in% c("true") & exportFile$extras.delivery_class %in% c("ussd")  & is.na(exportFile$extras.raw_user_address), ]
 
 #check
-recontact[sample(nrow(recontact), 30), c("extras.raw_user_address", "extras.is_registered")]
+recontact[sample(nrow(recontact), 30), c("extras.raw_user_address", "extras.is_registered", "extras.delivery_class")]
 
 numberstoSample <- recontact$msisdn
 
 groups <- 3
-elements <- 5000
+elements <- ifelse( nrow(recontact) < 10000, floor(nrow(recontact)/3), 3333)
+
 
 set.seed(20140413)
-recontactSample <- as.data.frame(matrix(sample(numberstoSample,size=(groups*elements)),nrow=elements, ncol=groups))
-names(recontactSample) <-  c("group1", "group2", "group3")
-                                
-#WriteOutput
+recontactSample <- as.data.frame(matrix(sample(numberstoSample,size=(groups*elements)),nrow=elements*groups, ncol=1))
+names(recontactSample) <-  c("numbers")
+recontactSample$group <- NA
+recontactSample[1:elements, "group" ] <- "group1"
+recontactSample[(elements+1):(elements+3334), "group"] <- "group2"
+recontactSample[(elements*2+1):nrow(recontactSample),"group" ] <- "group3"
+
+#> nrow(recontactSample[recontactSample$group%in% c("group1"),])
+
+
+write.csv(recontactSample, file=paste0("recontactSample", Sys.Date(), ".csv"))
+
+setwd("..")
